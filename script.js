c65c5cdf3efb4d4689f6103650f077994279653b
Heather Booker <heather.booker@hotmail.com>
2015-05-24 15:07:08 -0700

    simple calendar for may

---
+++ script.js
@@ -0,0 +1,9 @@
+function inform(day) {
+    if (day === 1) {
+        alert('Sunday:\nBegin with 5 minutes of bachata research/practice daily.')
+    } else if (day === 'thurs') {
+        alert('Thursday:\nBachata classes begin today at Azucar.')
+    } else if (day === 'firstSat') {
+        alert('Saturday:\nDaily research/practice increases to 5 minutes, two times daily.')
+    }
+}
